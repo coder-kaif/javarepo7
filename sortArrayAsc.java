1f98e8467dd33b310940f2862ae584f6c69f42f4
Kaif Ansari <115021353+coder-kaif@users.noreply.github.com>
2022-10-04 21:14:55 +0530

    Add files via upload
    Approach: An ArrayList can be Sorted by using the sort() method of the Collections Class in Java. This sort() method takes the collection to be sorted as the parameter and returns a Collection sorted in the Ascending Order by default.

---
+++ sortArrayAsc.java
@@ -0,0 +1,31 @@
+// Java program to demonstrate
+// How to sort ArrayList in ascending order
+
+import java.util.*;
+
+public class sortArrayAsc {
+	public static void main(String args[])
+	{
+
+		// Get the ArrayList
+		ArrayList<String>
+			list = new ArrayList<String>();
+
+		// Populate the ArrayList
+		list.add("Zain");
+		list.add("Khan");
+		list.add("ZainKhan");
+		list.add("ZainKhanZain");
+		list.add("A computer portal");
+
+		// Print the unsorted ArrayList
+		//missing
+
+		// Sorting ArrayList in ascending Order
+		// using Collection.sort() method
+		Collections.sort(list);
+
+		// Print the sorted ArrayList
+		System.out.println("Sorted ArrayList "+ "in Ascending order : "+ list);
+	}
+}
